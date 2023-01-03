c57fc5d27cf5356ef5cb3a2bfe6fab5323aba5ef
Darynka Tapia <darytapia09@gmail.com>
2023-01-03 14:38:47 -0600

    use sm for mobile desing

--- src/components/rock-paper-scissors.vue
+++ src/components/rock-paper-scissors.vue
@@ -17,101 +17,81 @@ import TheRock from '@/assets/images/icon-rock.svg'
       </div>
     </header>
     <main>
-      <the-item-circle 
-        :borderColor="'#5870f5'" 
-        borderColorShadow="#2c47c3"
-        :imagePath="ThePaper"
-      />
-      <the-item-circle 
-        :borderColor="'#eca019'" 
-        borderColorShadow="#c66c19"
-        :imagePath="TheScissors"
-      />
-      <the-item-circle 
-        :borderColor="'#d93852'" 
-        :borderColorShadow="'#9a182f'"
-        :imagePath="TheRock"
-        class="rock"
-      />
-    </main>
-    <footer>
+      <p class="message">Select your option for playing</p>
+      <div class="rps-options">
+        <the-item-circle 
+          :borderColor="'#5870f5'" 
+          borderColorShadow="#2c47c3"
+          :imagePath="ThePaper"
+        />
+        <the-item-circle 
+          :borderColor="'#eca019'" 
+          borderColorShadow="#c66c19"
+          :imagePath="TheScissors"
+        />
+        <the-item-circle 
+          :borderColor="'#d93852'" 
+          :borderColorShadow="'#9a182f'"
+          :imagePath="TheRock"
+          class="rock"
+        />
+      </div>
+      
+    <div class="rules">
       <button> rules </button>
-    </footer>
+    </div>
+    </main>
   </div>
   
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap');
 .game-container{
+  @apply w-full h-full py-10 px-0.5;
   font-family: 'Barlow Semi Condensed', sans-serif;
   background-image: var(--radial-gradient);
-  width: 100%;
-  height: 100vh;
-  padding: 40px 2px;
-}
-header{
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  max-width: 500px;
-  height: auto;
-  border: 2px solid var(--header-outline);
-  padding: 12px 14px;
-  margin: 0 auto;
-  border-radius: 10px;
-}
-.score{
-  background: white;
-  height: 80px;
-  width: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  border-radius: 10px;
-}
-.score__title{
-  font-size: 12px;
-  color: var(--score-text);
-  text-transform: uppercase;
-}
-.score__number{
-  color: var(--dark-text);
-  font-size: 44px;
-  font-weight: 700;
-}
-
-main{
-  margin: 50px auto;
-  width: 100%;
-  max-width: 600px;
-  display: grid;
-  height: 60vh;
-  align-items: center;
-  justify-items: center;
-  grid-template-columns: repeat(2,minmax(0,1fr));
-  grid-template-rows: repeat(2,minmax(0,1fr));
+  & header{
+    @apply flex justify-between w-full h-auto py-3 px-4 my-0 mx-auto rounded-xl;
+    max-width: 500px;
+    border: 2px solid var(--header-outline);
+    & .score{
+      @apply bg-white w-24 h-20 flex justify-center items-center flex-col rounded-xl;
+      & .score__title{
+        @apply text-xs uppercase;
+        color: var(--score-text);
+      }
+      & .score__number{
+        @apply text-4xl font-bold;
+        color: var(--dark-text);
+      }
+    }
+  }
+  & main{
+    @apply flex flex-col justify-between;
+    height: calc(100% - 94px);
+    & .message{
+      @apply text-center my-4 text-2xl text-white;
+    }
+    & .rps-options{
+      @apply m-auto w-full grid items-center justify-items-center;
+      max-width: 600px;
+      grid-template-columns: repeat(2,minmax(0,1fr));
+      grid-template-rows: repeat(2,minmax(0,1fr));
+      & .rock{
+        grid-column: 1 / 3;
+        grid-row: 2 / 3;
+      }
+    }
+    & .rules {
+      @apply flex justify-end py-0 px-10; 
+      & button {
+        @apply text-base uppercase bg-transparent text-white py-2 px-6 rounded border-2 border-solid border-white;
+        font-family: 'Barlow Semi Condensed', sans-serif;
+      }
+    }
+  }
 }
 
-main > .rock{
-  grid-column: 1 / 3;
-  grid-row: 2 / 3;
-}
 
-footer {
-  display: flex;
-  justify-content: flex-end;
-  padding: 0 40px;
-}
-footer button {
-  font-family: 'Barlow Semi Condensed', sans-serif;
-  font-size: 16px;
-  text-transform: uppercase;
-  background: transparent;
-  border: 2px solid white;
-  color: white;
-  padding: 8px 22px;
-  border-radius: 4px;
-}
 </style>
--- src/components/the-item-circle.vue
+++ src/components/the-item-circle.vue
@@ -13,31 +13,17 @@ const props = defineProps(['borderColor', 'borderColorShadow', 'imagePath'])
 
 <style lang="scss" scoped>
 .item{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 200px;
-  height: 200px;
-  background: white;
-  border-radius: 50%;
+  @apply flex justify-center items-center w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] bg-white rounded-full;
   border: 22px solid v-bind(borderColor);
   box-shadow: 2px 8px 2px -0px v-bind(borderColorShadow);
-}
-.item .item-rounded{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 156px;
-  height: 156px;
-  border-radius: 50%;
-
-  -webkit-box-shadow: inset 0px 8px 25px 0px rgba(0,0,0,0.75);
-  -moz-box-shadow: inset 0px 8px 25px 0px rgba(0,0,0,0.75);
-  box-shadow: inset 0px 8px 25px 0px rgba(0,0,0,0.75);
-}
-.item img{
-  width: 75px;
-  height: 75px;
-  object-fit: contain;
+  & .item-rounded{
+    @apply flex justify-center items-center w-[107px] h-[107px] sm:w-[156px] sm:h-[156px] rounded-full;
+    -webkit-box-shadow: inset 0px 8px 25px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: inset 0px 8px 25px 0px rgba(0,0,0,0.75);
+    box-shadow: inset 0px 8px 25px 0px rgba(0,0,0,0.75);
+  }
+  & img{
+    @apply w-16 h-16 object-contain;
+  }
 }
 </style>
